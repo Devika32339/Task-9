b277e186f3fc522df34f23ba990782cd91675fc0
Devika32339 <devikaa20112002@gmail.com>
2025-10-07 22:13:46 +0530

    Create project.py

---
+++ project.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Simple Python script for Task Three
+
+def greet(qwerty):
+    print(f"Hello, {qwerty}! Welcome to Task Three.")
+
+if __name__ == "__main__":
+    user_name = input("qwerty: ")
+    greet(user_name)
